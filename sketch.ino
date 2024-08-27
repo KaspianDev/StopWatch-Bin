cf8a3972e0b71e8a693b6dd0ca3cfec37131c39f
Kaspian <34658474+KaspianDev@users.noreply.github.com>
2024-08-28 01:09:13 +0200

    Finish timer

--- sketch.ino
+++ sketch.ino
@@ -1,30 +1,53 @@
 #include "TM1637.h"
 
 TM1637 display;
-int stopwatchTime = 0;
+int stopwatchTime = -1;
+int hours = 0;
 
 void setup() {
   Serial1.begin(115200);
   Serial1.println("Hello, Raspberry Pi Pico W!");
   display.begin(2, 3, 4);
-  display.displayTime(00, 00, true);
-  
+
   pinMode(16, INPUT); // Button
   pinMode(15, OUTPUT); // Buzzer
 }
 
-void loop() {
-  int newValue = digitalRead(16);
+void count() {
+  stopwatchTime++;
 
-Serial1.println(newValue);
-  if (newValue == HIGH) {
-    if (stopwatchTime == 0) {
-      stopwatchTime = 1;
-      tone(15, 150, 50);
+  updateTime();
+}
+
+void updateTime() {
+  int minutes = stopwatchTime / 60;
+  if (minutes > 59) {
+    stopwatchTime = 1;
+  }
+
+  int seconds = stopwatchTime % 60;
+  display.displayTime(minutes, seconds, true);
+}
+
+int lastState = HIGH;
+void loop() {
+  int value = digitalRead(16);
+  if (lastState != value) {
+    lastState = value;
+    if (stopwatchTime == -1) {
+      stopwatchTime = 3580;
+      tone(15, 180, 30);
     } else {
       stopwatchTime = 0;
+      updateTime();
+      stopwatchTime = -1;
+      tone(15, 160, 30);
     }
   }
 
-  delay(100);
+  delay(1);
+  if (stopwatchTime > -1) {
+    count();
+    delay(999);
+  }
 }
