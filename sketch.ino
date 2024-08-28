3acae162c53c163461255ddf8b86db5745e3483a
KaspianDev <kacperleaguenine@gmail.com>
2024-08-28 12:06:15 +0200

    Add debouncing checks

--- sketch.ino
+++ sketch.ino
@@ -3,8 +3,7 @@
 TM1637 display;
 int stopwatchTime = -1;
 
-int hours = 0;
-
+const long DEBOUNCE_DELAY = 150;
 const int START_PIN = 27;
 const int ALARM_PIN = 28;
 
@@ -13,13 +12,23 @@ void setup() {
   Serial1.println("Hello, Raspberry Pi Pico W!");
   display.begin(2, 3, 4);
 
-  pinMode(START_PIN, INPUT); // Button
+  pinMode(START_PIN, INPUT); // Start/Stop Button
+  pinMode(ALARM_PIN, INPUT); // Alarm Button
   pinMode(15, OUTPUT); // Buzzer
+
+  incrementAlarm();
+}
+
+int alarmMinutes = -1;
+
+void incrementAlarm() {
+  alarmMinutes++;
+  display.displayTime(alarmMinutes, 00, true);
 }
 
 int milis = 0;
 void count() {
-  if (milis < 1000) {
+  if (milis < 999) {
     milis++;
     return;
   }
@@ -39,21 +48,38 @@ void updateTime() {
   display.displayTime(minutes, seconds, true);
 }
 
-int lastState = 0;
+int lastOnOffState = 0;
+long lastOnOffDebounce = 0;
+
+int lastAlarmState = 0;
+long lastAlarmDebounce = 0;
 void loop() {
   delay(1);
 
-  int value = digitalRead(START_PIN);
-  if (lastState != value) {
-    lastState = value;
-    if (stopwatchTime == -1) {
-      stopwatchTime = 0;
-      tone(15, 180, 30);
-    } else {
-      stopwatchTime = 0;
-      updateTime();
-      stopwatchTime = -1;
-      tone(15, 160, 30);
+  int onOffValue = digitalRead(START_PIN);
+  if (lastOnOffState != onOffValue) {
+    lastOnOffState = onOffValue;
+    if ((millis() - lastOnOffDebounce) > DEBOUNCE_DELAY) {
+      if (stopwatchTime == -1) {
+        stopwatchTime = 0;
+        tone(15, 180, 30);
+      } else {
+        stopwatchTime = 0;
+        updateTime();
+        stopwatchTime = -1;
+        tone(15, 160, 30);
+      }
+      lastOnOffDebounce = millis();
+    }
+  }
+
+  int alarmState = digitalRead(ALARM_PIN);
+  if (lastAlarmState != alarmState) {
+    lastAlarmState = alarmState;
+    if ((millis() - lastAlarmDebounce) > DEBOUNCE_DELAY) {
+      incrementAlarm();
+      tone(15, 170, 10);
+      lastAlarmDebounce = millis();
     }
   }
 
